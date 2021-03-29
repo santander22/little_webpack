c51e67e8f6271367fa8dfd7aa6666840cb083b7f
Santan <zerausgogo@gmail.com>
2021-03-29 08:50:47 -0600

    optimize css and javascript on webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
+        filename: "[name].[contenthash].js",
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     resolve: {
@@ -42,7 +44,7 @@ module.exports = {
                 options: {
                     limit: 10000,
                     mimetype: "application/font-woff",
-                    name: "[name].[ext]",
+                    name: "[name].[contenthash].[ext]",
                     outputPath: "./assets/fonts/",
                     publicPath: "./assets/fonts/",
                     esModule: false
@@ -57,7 +59,9 @@ module.exports = {
             template: './public/index.html',
             filename: './index.html'
         }),
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "assets/[name].[contenthash].css"
+        }),
         new CopyPlugin({
             patterns: [
                 {
@@ -66,5 +70,12 @@ module.exports = {
                 }
             ]
         })
-    ]
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    }
 }
